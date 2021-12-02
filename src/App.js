512fe1a1ea78471c2f1755e232c85f1b27429cdb
vuong1mp <nhokjay99@gmail.com>
2021-12-03 00:37:08 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          minh vuong
+          minh vuong dep trai qua troi
         </p>
 
       </header>
